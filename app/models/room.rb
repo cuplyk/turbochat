8f24ffe3e3ec19dc8669684d0f2602663523951d
cuplyk <ursachi.g@mail.ru>
2022-08-04 13:47:45 +0200

    Add message model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
+    has_many :messages, -> { sorted }, dependent :destroy  #    dependent: :destroy means that when a room is deleted, all of its messages are deleted
     belongs_to :user # user is the owner of the room
     before_create {self.title = SecureRandom.hex(3)} # SecureRandom.hex(3) is a method that generates a random string of length 3
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :rooms, dependent: :destroy
+  has_many :rooms, dependent: :destroy # dependent: :destroy means that when a user is deleted, all of their rooms are deleted
+  has_many :messages, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
